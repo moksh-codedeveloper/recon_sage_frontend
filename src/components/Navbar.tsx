f017435f370ee1a45872d8c85f06a331bae3e574
Malde Moksh <maldemoksh437@gmail.com>
2026-01-08 18:29:36 +0530

    feat: add navigation menu and UI components; update global styles and dependencies
    - Added Navbar component for navigation.
    - Introduced Button, Card, and NavigationMenu components with respective styles.
    - Updated global CSS to include new design tokens and animations.
    - Integrated new dependencies: Radix UI for navigation and class variance authority for styling.
    - Refactored page layout to utilize the new Navbar component.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { Card } from "./ui/card";
+
+export default function Navbar(){
+    return (
+        <>
+            <div className="p-1 m-2">
+                Hello i am Navbar
+            </div>
+        </>
+    );
+}
