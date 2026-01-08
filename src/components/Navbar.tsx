a1d55bccf601c9aa4e51bad514bf1450a971c526
Malde Moksh <maldemoksh437@gmail.com>
2026-01-08 18:48:47 +0530

    feat: update Navbar component layout and remove unused navigation menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 "use client";
 import React from "react";
-import { Card } from "./ui/card";
-
-export default function Navbar(){
+import { Button } from "./ui/button";
+export default function Navbar() {
     return (
         <>
-            <div className="p-1 m-2">
-                Hello i am Navbar
-            </div>
+            <div className="p-3 m-5 h-10 max-w-full flex justify-center items-center">
+                <div className="bg-cyan-200 rounded-2xl p-3 border-cyan-300 mt-6">
+                    <Button className="m-3">Home</Button>
+                    <Button className="m-3">Contact us</Button>
+                    <Button className="m-3">About project</Button>
+                    <Button className="m-3">Premium version</Button>
+                </div>
+            </div >
         </>
     );
 }
