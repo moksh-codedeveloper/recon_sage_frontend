058ad2cf1b8d8373f7ec6a3f66aa3279021907be
Malde Moksh <maldemoksh437@gmail.com>
2026-01-08 20:12:54 +0530

    feat: enhance Navbar styling and structure; update Home component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import { Card, CardHeader, CardDescription, CardContent } from "@/components/ui/card";
 export default function Home() {
   return (
     <>
-      <Navbar/>
+      <div className="mb-4">
+        <Navbar />
+      </div>
+      <div className="mt-5 flex justify-center">
+        <Card className="mt-10 w-100 absolute">
+          <CardHeader className="text-xl text-cyan-500">ReconSage</CardHeader>
+          <CardContent className="text-zinc-400">Scanner which observes the target and then evaluate the behavior of it.</CardContent>
+        </Card>
+      </div>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,12 @@ import { Button } from "./ui/button";
 export default function Navbar() {
     return (
         <>
-            <div className="p-3 m-5 h-10 max-w-full flex justify-center items-center">
-                <div className="bg-cyan-200 rounded-2xl p-3 border-cyan-300 mt-6">
-                    <Button className="m-3">Home</Button>
-                    <Button className="m-3">Contact us</Button>
-                    <Button className="m-3">About project</Button>
-                    <Button className="m-3">Premium version</Button>
+            <div className="p-3 m-5 h-10 flex justify-center items-center sticky">
+                <div className="bg-cyan-200 rounded-3xl p-3 border-cyan-300 mt-6">
+                    <Button className="m-3 rounded-3xl">Home</Button>
+                    <Button className="m-3 rounded-3xl">Contact us</Button>
+                    <Button className="m-3 rounded-3xl">About project</Button>
+                    <Button className="m-3 rounded-3xl">Premium version</Button>
                 </div>
             </div >
         </>
